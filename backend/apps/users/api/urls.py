414a4bb023c8876393804c223b5a897cc8a13150
guilherme.dantas <guilherme.dantas@miningmath.com>
2026-05-26 18:47:54 -0300

    fix: adiciona endpoints CRUD para perfil de usuário e atualiza specs

--- backend/apps/users/api/urls.py
+++ backend/apps/users/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView
+from .views import RegisterView, ProfileView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='user-register'),
+    path('me/', ProfileView.as_view(), name='user-profile'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
